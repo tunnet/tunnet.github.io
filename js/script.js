ff02a18e4cd432345e9d02e95e3491bb37716488
Web Development <59807095+tunnet@users.noreply.github.com>
2020-01-31 23:49:14 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+
+
+	jQuery(function($){
+	   $("#phone").mask("389999999999");
+	});
+
+	$("#bd-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('html, body').animate({scrollTop: top}, 1500);
+    });
+
+
+	$(".banner-form").on('submit', function() {
+		event.preventDefault();
+		$.ajax({
+			url: 'mail.php',
+			data: $(this).serialize(),
+			type: 'POST'
+		})
+		.done(function(response) {
+			console.log(response);
+			console.log('done');
+			openSucsessPopup();
+			setTimeout(function(){
+			  $.magnificPopup.close();
+			}, 1000);
+		})
+		.fail(function(xhr, status, errorThrown) {
+			console.log('Error:', errorThrown, 'status', status);
+			console.dir(xhr);
+			openFailPopup();
+			setTimeout(function(){
+			  $.magnificPopup.close();
+			}, 1000);
+		})
+	});
+
+	$('.contact__btn').magnificPopup({
+		type: 'inline',
+		preloader: false,
+	});
+
+
+	setTimeout(function(){
+	  $.magnificPopup.open({
+			items: { src: '#popup' },
+			type: 'inline',
+			fixedContentPos: true,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			focus: '#name',
+			mainClass: 'my-mfp-slide-bottom'
+		});
+	}, 10000);
+
+	// Объявление попап-функций. Плагин Magnific Popup
+	var openSucsessPopup = function() {
+		$.magnificPopup.open({
+			items: { src: '#success-popup' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-slide-bottom'
+		});
+	};
+
+	var openFailPopup = function() {
+		$.magnificPopup.open({
+			items: { src: '#fail-popup' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: true,
+			midClick: false,
+			removalDelay: 300,
+			mainClass: 'my-mfp-slide-bottom'
+		});
+	};
+
+
+	$('#callback').on('submit', function() {
+		event.preventDefault();
+		$.ajax({
+			url: 'mail.php',
+			data: $(this).serialize(),
+			type: 'POST'
+		})
+		.done(function(response) {
+			console.log(response);
+			console.log('done');
+			openSucsessPopup();
+			setTimeout(function(){
+			  $.magnificPopup.close();
+			}, 1000);
+		})
+		.fail(function(xhr, status, errorThrown) {
+			console.log('Error:', errorThrown, 'status', status);
+			console.dir(xhr);
+			openFailPopup();
+			setTimeout(function(){
+			  $.magnificPopup.close();
+			}, 1000);
+		})
+	});
+});
